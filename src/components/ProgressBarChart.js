00c5ed01247a25d69742cfc4af8a954ab21ab41b
GretchS <gretchen.scott@hotmail.com>
2018-01-19 13:29:34 +1100

    Tooltip renders Bib number, distance and time on hover. Still doesn't render musher name.

--- src/components/ProgressBarChart.js
+++ src/components/ProgressBarChart.js
@@ -4,6 +4,7 @@ import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Cell, Re
 import PropTypes from 'prop-types';
 import { getPastMushers } from '../api/pastmushers'
 
+/*
 const generateYearsArray = () => {
   let years = []
   for (let i = 2000; i < (new Date()).getFullYear(); i++) {
@@ -25,7 +26,7 @@ const determineExperience = (data, id) => {
     })
   return vetranMushers
 }
-
+*/
 
   
 const renderLegend = () => {
@@ -74,9 +75,11 @@ class CustomTooltip extends Component{
     type: PropTypes.string,
     payload: PropTypes.array,
     bib: PropTypes.string,
+    name: PropTypes.string,
   }
 
-
+ 
+/*
   componentDidMount() {
     getPastMushers().then((res) => {
       this.setState({ data: res })
@@ -84,18 +87,16 @@ class CustomTooltip extends Component{
       this.setState({ data: determineExperience(this.state.mushers) })
     })
   }
-
+*/
   render() {
     const { active } = this.props;
 
     if (active) {
-      const { payload } = this.props;
+      const { payload, label, name } = this.props;
       return (
+        console.log(payload),
         <div className="custom-tooltip">
-          <p className="desc">Distance:</p>
-          <p className="label">         
-          {` ${payload[2].name}`}</p>
-          <p className="intro">{`${payload.name(label)}`}</p>
+        <p className="label"> { ` Bib (${payload[0].payload.bib}), Distance: ${label}, Time: ${payload[0].payload.time} `}</p>  
         </div>
       );
     }
@@ -111,7 +112,7 @@ const series = [
         {name: 'Cholena', data: [
           {bib: '1', dist: 0, time: 0},
           {bib: '1', dist: 100, time: 13},
-          {bib: '1', dist: 150, time: 22}
+          {bib: '1', dist: 166, time: 22}
         ]},
         {name: 'Gretch', data: [
           {bib: '2', dist: 0, time: 0},
@@ -122,7 +123,7 @@ const series = [
         ]},
         {name: 'Hannah', data: [
           {bib: '3', dist: 0, time: 0},
-          {bib: '3', dist: 17, time: 3},
+          {bib: '3', dist: 99, time: 21},
           {bib: '3', dist: 224, time: 28}
         ]},
           {name: 'Carl', data: [
@@ -133,94 +134,94 @@ const series = [
         {name: 'Pat', data: [
           {bib: '5', dist: 0, time: 0},
           {bib: '5', dist: 16, time: 12},
-          {bib: '5', dist: 22, time: 18},
-          {bib: '5', dist: 21, time: 22},
+          {bib: '5', dist: 29, time: 18},
+          {bib: '5', dist: 131, time: 22},
           {bib: '5', dist: 331, time: 29}
         ]},
         {name: 'Matt', data: [
           {bib: '6', dist: 0, time: 0},
           {bib: '6', dist: 17, time: 3},
-          {bib: '6', dist: 24, time: 28}
+          {bib: '6', dist: 200, time: 28}
         ]},
         {name: 'Fholena', data: [
           {bib: '7', dist: 0, time: 0},
           {bib: '7', dist: 100, time: 13},
-          {bib: '7', dist: 150, time: 22}
+          {bib: '7', dist: 138, time: 22}
         ]},
         {name: 'Fretch', data: [
           {bib: '8', dist: 0, time: 0},
           {bib: '8', dist: 16, time: 12},
-          {bib: '8', dist: 22, time: 18},
-          {bib: '8', dist: 21, time: 22},
-          {bib: '8', dist: 31, time: 29}
+          {bib: '8', dist: 90, time: 18},
+          {bib: '8', dist: 111, time: 22},
+          {bib: '8', dist: 200, time: 29}
         ]},
         {name: 'Fannah', data: [
           {bib: '9', dist: 0, time: 0},
-          {bib: '9', dist: 17, time: 3},
+          {bib: '9', dist: 86, time: 9},
           {bib: '9', dist: 328, time: 28}
         ]},
           {name: 'Farl', data: [
           {bib: '10', dist: 0, time: 0},
-          {bib: '10', dist: 100, time: 13},
-          {bib: '10', dist: 150, time: 22}
+          {bib: '10', dist: 136, time: 13},
+          {bib: '10', dist: 170, time: 22}
         ]},
         {name: 'Fat', data: [
           {bib: '11', dist: 0, time: 0},
-          {bib: '11', dist: 16, time: 12},
-          {bib: '11', dist: 22, time: 18},
-          {bib: '11', dist: 21, time: 22},
-          {bib: '11', dist: 181, time: 29}
+          {bib: '11', dist: 106, time: 12},
+          {bib: '11', dist: 202, time: 18},
+          {bib: '11', dist: 215, time: 22},
+          {bib: '11', dist: 319, time: 29}
         ]},
         {name: 'Fatt', data: [
           {bib: '12', dist: 0, time: 0},
-          {bib: '12', dist: 17, time: 3},
+          {bib: '12', dist: 14, time: 3},
           {bib: '12', dist: 224, time: 28}
         ]},
         {name: 'Gat', data: [
           {bib: '13', dist: 0, time: 0},
           {bib: '13', dist: 16, time: 12},
-          {bib: '13', dist: 22, time: 18},
-          {bib: '13', dist: 21, time: 22},
+          {bib: '13', dist: 29, time: 18},
+          {bib: '13', dist: 52, time: 22},
           {bib: '13', dist: 231, time: 29}
         ]},
         {name: 'Gatt', data: [
           {bib: '14', dist: 0, time: 0},
           {bib: '14', dist: 17, time: 3},
-          {bib: '14', dist: 24, time: 28}
+          {bib: '14', dist: 204, time: 28}
         ]},
         {name: 'Gholena', data: [
           {bib: '15', dist: 0, time: 0},
-          {bib: '15', dist: 100, time: 13},
-          {bib: '15', dist: 150, time: 22}
+          {bib: '15', dist: 79, time: 13},
+          {bib: '15', dist: 142, time: 22}
         ]},
         {name: 'Ggretch', data: [
           {bib: '16', dist: 0, time: 0},
           {bib: '16', dist: 16, time: 12},
           {bib: '16', dist: 22, time: 18},
-          {bib: '16', dist: 21, time: 22},
-          {bib: '16', dist: 31, time: 29}
+          {bib: '16', dist: 221, time: 22},
+          {bib: '16', dist: 301, time: 29}
         ]},
         {name: 'Gannah', data: [
           {bib: '17', dist: 0, time: 0},
-          {bib: '17', dist: 17, time: 3},
+          {bib: '17', dist: 107, time: 3},
           {bib: '17', dist: 328, time: 28}
         ]},
           {name: 'Garl', data: [
           {bib: '18', dist: 0, time: 0},
-          {bib: '18', dist: 100, time: 13},
-          {bib: '18', dist: 150, time: 22}
+          {bib: '18', dist: 68, time: 13},
+          {bib: '18', dist: 193, time: 22}
         ]},
         {name: 'Hat', data: [
           {bib: '19', dist: 0, time: 0},
           {bib: '19', dist: 16, time: 12},
           {bib: '19', dist: 22, time: 18},
-          {bib: '19', dist: 21, time: 22},
-          {bib: '19', dist: 31, time: 29}
+          {bib: '19', dist: 89, time: 22},
+          {bib: '19', dist: 312, time: 29}
         ]},
         {name: 'Hatt', data: [
           {bib: '20', dist: 0, time: 0},
-          {bib: '20', dist: 17, time: 3},
-          {bib: '20', dist: 24, time: 28}
+          {bib: '20', dist: 66, time: 3},
+          {bib: '20', dist: 240, time: 28}
         ]},
       ];
 
@@ -228,26 +229,10 @@ class ProgressBarChart extends Component{
   constructor(props) {
     super(props);
 
-  propTypes= {
-    type: PropTypes.string,
-    payload: PropTypes.array,
-    bib: PropTypes.string,
   }
+   
 
-  state = {
-      mushers: null,
-      data: null
-    }
-  }
-    componentDidMount() {
-      getMushers().then((res) => {
-        this.setState({ mushers: res })
-      }).then(() => {
-        this.setState({ data: generateProgressDistanceData(this.state.mushers) })
-      })
-    }
-
-    
+ /*   
 const generateKeyArray = (data, filterKey) => {
   let countArray = []
   data.forEach((datum) => {
@@ -279,7 +264,7 @@ const generateData = (data, key) => {
   })
   return filteredData
 }
-
+*/
 
   render () {
     const payload = this.props;
@@ -294,11 +279,11 @@ const generateData = (data, key) => {
               <Label value="Distance" offset={-15} position="insideBottom" />
             </XAxis>
 
-            <YAxis type="category" dataKey="bib" name={payload.name} domain={[20, 0]}>
+            <YAxis type="category" dataKey="bib" name={payload.value} domain={[20, 0]}>
               <Label value="Name" angle={-90} offset={-15} position="insideLeft" style={{ textAnchor: 'middle' }} />
              </YAxis>
             
-             <Tooltip content={<CustomTooltip/>}/>
+             <Tooltip viewBox={{ x: 0, y: 0, width: 200, height: 400 }} content={<CustomTooltip/>}/>
           
 
               {series.map(s => (
